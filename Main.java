3f2b135fdd0b8d4fc9ba7a0f36776b80405b0618
Leonardo <162130975+Pableox21@users.noreply.github.com>
2025-04-11 08:51:38 -0400

    11/4/25

--- Main.java
+++ Main.java
@@ -1,6 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        ejercicio1 ejercicio1=new ejercicio1();
-        ejercicio1.ejecutar();
+        //ejercicio1 ejercicio1=new ejercicio1();
+        //ejercicio1.ejecutar();
+        //ejercicio2 ejercicio2=new ejercicio2();
+        //ejercicio2.ejecutar();
+        //ejercicio3 ejercicio3=new ejercicio3();
+        //ejercicio3.ejecutar();
+        //ejercicio4 ejercicio4=new ejercicio4();
+        //ejercicio4.ejecutar();
+        //ejercicio5 ejercicio5=new ejercicio5();
+        //ejercicio5.ejecutar();
+        //ejercicio6 ejercicio6=new ejercicio6();
+        //ejercicio6.ejecutar();
+        //ejercicio7 ejercicio7=new ejercicio7();
+        //ejercicio7.ejecutar();
+        //ejercicio8 ejercicio8=new ejercicio8();
+        //ejercicio8.ejecutar();
+        //ejercicio9 ejercicio9=new ejercicio9();
+        //ejercicio9.ejecutar();
+        //ejercicio10 ejercicio10=new ejercicio10();
+        //ejercicio10.ejecutar();
+        ejercicio11 ejercicio11=new ejercicio11();
+        ejercicio11.ejecutar();
     }
 }
